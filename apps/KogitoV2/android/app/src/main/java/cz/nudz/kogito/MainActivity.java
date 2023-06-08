399a37be369a9bc028d903aa5d46af6d37105585
Martin Kraina <kraina.martin@gmail.com>
2023-06-08 20:47:29 +0200

    rename KogitoV2 to Kogito and use real bundleId & appId

--- apps/KogitoV2/android/app/src/main/java/cz/nudz/kogito/MainActivity.java
+++ apps/KogitoV2/android/app/src/main/java/cz/nudz/kogito/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kogitov2;
+package cz.nudz.kogito;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "KogitoV2";
+    return "Kogito";
   }
 
   @Override
